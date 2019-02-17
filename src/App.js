5f723e34fb2cf11f7b11e61133743fba31c71a42
BettyM <bettymp81@gmail.com>
2019-02-17 00:21:46 -0700

    Add PizzaSizes component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import { ApolloProvider } from "react-apollo"
+import ApolloClient from 'apollo-boost'
 import './App.css'
 import PizzaApp from './components'
+
+const client = new ApolloClient({
+  uri: "https://core-graphql.dev.waldo.photos/pizza"
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PizzaApp />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <PizzaApp />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- src/components/PizzaSizes/index.js
+++ src/components/PizzaSizes/index.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react'
+import { Query } from "react-apollo"
+import gql from "graphql-tag"
+import Checkbox from '@material-ui/core/Checkbox'
+import Grid from '@material-ui/core/Grid'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormGroup from '@material-ui/core/FormGroup'
+
+const SIZE_QUERY = gql`
+  {
+    pizzaSizes {
+      name
+      maxToppings
+      basePrice
+    }
+  }
+`
 
 export default class PizzaSizes extends Component {
-  render() {
-    return(
-      <div>
-        Pizza Sizes
+  getLabel(data) {
+    const maxToppingLabel = data.maxToppings ?
+      `1 to ${data.maxToppings} Ingredients` : 'No limit of ingredients'
+
+    return (
+      <div className="size-label">
+        <div className="font-normal">{data.name}</div>
+        <div className="font-disabled">{maxToppingLabel}</div>
+        <div className="font-small">${data.basePrice}</div>
       </div>
+    )
+  }
+
+  handleSizeChange = e => {
+    console.log('size change', e.target)
+  }
+
+  render() {
+   return(
+      <Query query={SIZE_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>LOADING...</div>
+        if (error) return <div>ERROR</div>
+        return (
+          <div className="sizes-section">
+          <Grid container justify="center" spacing={40}>
+            <FormGroup row>
+              {data.pizzaSizes.map((pizza, index) =>
+                <Grid item key={index}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                    checked={false}
+                    onChange={e => this.handleSizeChange(e)}
+                    value={pizza.name} />
+                  }
+                  label={this.getLabel(pizza)}
+                  key={pizza.name}
+                />
+                </Grid>
+              )}
+            </FormGroup>
+          </Grid>
+          </div>
+        )
+      }}
+      </Query>
     )
   }
 }
